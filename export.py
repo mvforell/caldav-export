4f1ab4194cedced6ba1f404988fee906db6cf663
Max von Forell <max@vonforell.de>
2020-09-12 21:49:01 +0200

    Remove unnecessary imports, add calendar_name.txt for importing

--- export.py
+++ export.py
@@ -1,12 +1,13 @@
 import caldav
 import getpass
 
-from datetime import date, datetime, timedelta, timezone
-from os import getcwd, mkdir
+from datetime import date, datetime
+from os import mkdir
 from os.path import isdir, join
 from sys import exit
 
-EXPORT_FOLDER = 'ical_export'
+EXPORT_FOLDER_PREFIX = 'ical_export'
+CAL_NAME_FILE = 'calendar_name.txt'
 
 def main():
     client = caldav.DAVClient(
@@ -36,6 +37,8 @@ def main():
     for cal in principal.calendars():
         for ev in cal.date_search(start=start, end=end, expand=True): # expand recurring events
             write_event(cal.name, ev)
+        with open(join(calendar_folder(cal.name), CAL_NAME_FILE), 'w') as f: # for importing
+            f.write(cal.name)
         print(f'Exported events from calendar \'{cal.name}\'.')
 
 def timeinput(prompt, default):
@@ -50,11 +53,14 @@ def timeinput(prompt, default):
 def write_event(calendar_name, event):
     url = str(event.url)
     filename = url[url.rfind('/') + 1:]
-    folder = join(getcwd(), f'{EXPORT_FOLDER}_{calendar_name.replace(" ", "_")}')
+    folder = calendar_folder(calendar_name)
     if not isdir(folder):
         mkdir(folder)
     with open(join(folder, filename), 'w') as f:
         f.write(event.data)
+
+def calendar_folder(calendar_name):
+    return f'{EXPORT_FOLDER_PREFIX}_{calendar_name.replace(" ", "_")}'
 
 if __name__ == '__main__':
     try:
